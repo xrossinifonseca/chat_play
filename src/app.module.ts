390d2e19676cea20e0024f94b07038aa4e16984f
Rossini fonseca <rossinidrum@outlook.com>
2023-12-05 10:26:02 -0300

    feat: Importar AuthMOdule e GAtewayModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
+import { AuthModule } from './auth/auth.module';
+import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [CustomersModule, AuthModule, GatewayModule],
   controllers: [],
   providers: [],
 })
