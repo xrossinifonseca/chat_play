b67a4c15b9ac6a25b7547c55a1a88c5f1311ac37
Rossini fonseca <rossinidrum@outlook.com>
2023-12-05 10:12:38 -0300

    refactor: remover metodo login no customersController

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,13 +1,7 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { CustomersService } from './customers.service';
-import { CreateCustomerDto } from './customers-dto';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private readonly customersService: CustomersService) {}
-
-  @Post('create')
-  async create(@Body(new ValidationPipe()) data: CreateCustomerDto) {
-    return this.customersService.create(data);
-  }
 }
